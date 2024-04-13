06a80fadde72a76811a45c18aff33f4f53bb1b82
dannegm <im@danielgarcia.me>
2024-04-13 15:19:28 -0600

    Adding email template for notifications

--- src/domains/notifications/router.js
+++ src/domains/notifications/router.js
@@ -2,18 +2,46 @@ import 'dotenv/config';
 
 import { Router } from 'express';
 import { Resend } from 'resend';
+import { v4 as uuid } from 'uuid';
+
+import NotificationEmail from '@/emails/notification';
 
 const router = Router();
 
 const RESEND_API_KEY = process.env.RESEND_API_KEY;
 const resend = new Resend(RESEND_API_KEY);
 
+const SENDER_EMAIL = 'CronHook <no-reply@mail.hckr.mx>';
+
+const extractUserData = userEmail => {
+    const regex = /(.+?) <(.+?)>/;
+    const match = userEmail.match(regex);
+
+    if (!match) {
+        return null;
+    }
+
+    return { name: match[1], email: match[2] };
+};
+
 router.post('/notification', async (req, res) => {
     const { data, error } = await resend.emails.send({
-        from: req.body.from,
+        from: SENDER_EMAIL,
         to: req.body.to,
         subject: req.body.subject,
-        html: req.body.content,
+        react: NotificationEmail({
+            user: extractUserData(req.body.to),
+            subject: req.body.subject,
+            content: req.body.content,
+            icon: req.body.icon,
+            action: {
+                href: req.body?.action?.href,
+                label: req.body?.action?.label,
+            },
+        }),
+        headers: {
+            'X-Entity-Ref-ID': uuid(),
+        },
     });
 
     if (error) {
---
+++ src/emails/notification.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {
+    Html,
+    Body,
+    Head,
+    Preview,
+    Tailwind,
+    Container,
+    Section,
+    Img,
+    Hr,
+    Text,
+    Button,
+    Row,
+    Column,
+} from '@react-email/components';
+
+const CRON_HOOK_BASE_URL = 'https://cronhook.hckr.mx';
+
+export default function TestEmail({
+    // ...
+    subject,
+    content,
+    icon,
+    action,
+    user,
+}) {
+    const previewText = content;
+
+    return (
+        <Html>
+            <Head>
+                <title>CronHook Notification</title>
+            </Head>
+
+            <Preview>{previewText}</Preview>
+
+            <Tailwind>
+                <Body className='bg-white font-sans px-2 my-auto mx-auto font-sans p-2'>
+                    <Container className='bg-slate-950 rounded-xl p-4 pt-0 mx-auto p-[20px]'>
+                        <Section className='flex flex-row justify-center p-8'>
+                            <Img src={`${CRON_HOOK_BASE_URL}/img/logo.png`} width='240' />
+                        </Section>
+
+                        <Section className='bg-white rounded-lg px-4'>
+                            <Section className='my-4'>
+                                <Text className='text-2xl font-bold my-0'>
+                                    You got a notification
+                                </Text>
+                                <Text className='text-slate-700 my-0'>
+                                    Don't miss out all your alerts and updates
+                                </Text>
+                            </Section>
+
+                            <Hr />
+
+                            <Section className='pt-2 pb-4'>
+                                <Row>
+                                    {icon !== '' && (
+                                        <Column className='align-top pr-4'>
+                                            <Img
+                                                className='rounded-lg aspect-square object-contain'
+                                                src={icon}
+                                                width='64'
+                                                height='64'
+                                            />
+                                        </Column>
+                                    )}
+
+                                    <Column className='align-top' width='100%'>
+                                        <Text className='text-lg font-semibold my-0'>
+                                            {subject}
+                                        </Text>
+                                        <Text className='text-sm my-0'>{content}</Text>
+
+                                        {action?.href !== '' && action?.label !== '' && (
+                                            <Button
+                                                className='mt-4 inline-block w-max font-semibold rounded-lg bg-gray-200 px-3 py-2 text-sm'
+                                                href={action?.href}
+                                            >
+                                                {action?.label}
+                                            </Button>
+                                        )}
+                                    </Column>
+                                </Row>
+                            </Section>
+
+                            {user?.email && (
+                                <>
+                                    <Hr />
+
+                                    <Section className='mb-4'>
+                                        <Text className='my-0 text-slate-500'>
+                                            This automated notification was sent to{' '}
+                                            <b>{user?.email}</b> because you deployed a function.
+                                        </Text>
+                                    </Section>
+                                </>
+                            )}
+                        </Section>
+                    </Container>
+                </Body>
+            </Tailwind>
+        </Html>
+    );
+}
